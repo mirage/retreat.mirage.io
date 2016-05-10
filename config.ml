ce765ad7e387747ff7e09fae9d19a4698318c640
Hannes Mehnert <hannes@mehnert.org>
2016-05-10 13:47:01 +0100

    do not use get_mode

--- config.ml
+++ config.ml
@@ -7,9 +7,9 @@ let address addr nm gw =
 let server = address "198.167.222.204" "255.255.255.0" "198.167.222.1"
 
 let net =
-  match get_mode () with
-  | `Unix -> socket_stackv4 default_console [Ipaddr.V4.any]
-  | `Xen  -> direct_stackv4_with_static_ipv4 default_console tap0 server
+  if_impl Key.is_xen
+    (direct_stackv4_with_static_ipv4 default_console tap0 server)
+    (socket_stackv4 default_console [Ipaddr.V4.any])
 
 (* Shell commands to run at configure time *)
 type shellconfig = ShellConfig
