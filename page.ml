148f267b6ecb77635db09d2190582d10b8c36755
Mindy <meetup@yomimono.org>
2018-02-01 10:27:22 -0600

    how should I behave?

--- page.ml
+++ page.ml
@@ -36,6 +36,7 @@ and fix bugs.
    * Dietary restrictions
 * *Who should participate?* Everybody interested in advancing MirageOS.
 * *How big?* We have only limited space (30 people).  Selection will be done by various diversity criteria.
+* *How should I behave while there?* Be kind and empathetic to others; do not harrass or threaten anyone. If you make others unsafe, you may be asked to leave.
 
 <sup>&#9733;</sup>: If you cannot afford this, please contact us directly (at <retreat2018@nqsb.io>).
 
