240284c295eeaf6540b11f02582d5b6f5aaf05e6
Hannes Mehnert <hannes@mehnert.org>
2018-11-28 17:45:20 +0100

    7th

--- page.ml
+++ page.ml
@@ -20,18 +20,19 @@ let header t =
 
 let content =
   Omd.to_html (Omd.of_string
-{___|# 6th MirageOS hack retreat
+{___|# 7th MirageOS hack retreat
 
-We invite you to participate in the sixth [MirageOS](https://mirage.io)
+We invite you to participate in the seventh [MirageOS](https://mirage.io)
 hack retreat!
 The goal is to sync various MirageOS subprojects, start new ones,
 and fix bugs.
 
-* *When?* 3rd October (arrival) - 10th October (departure) 2018
+* *When?* March 6th (arrival) - March 13th (departure) 2019
 * *Where?* Marrakech, Morocco at [Priscilla, Queen of the Medina](http://queenofthemedina.com/en/index.html).
 * *How much?* 350 EUR<sup>&#9733;</sup>, accommodation and food (full board) included.
-* *How do I register?* Register by sending a mail to <retreat2018@nqsb.io> **by August 31st, 2018** including:
-   * OCaml and MirageOS experience;
+* *How do I register?* Register by sending a mail to <retreat2018@nqsb.io> **by January 15th, 2019** including:
+   * How you became interested in MirageOS;
+   * Previous experience with MirageOS and OCaml (no upfront experience required) ;
    * Project(s) you're interested to work on; and
    * Dietary restrictions
 * *Who should participate?* Everybody interested in advancing MirageOS.
@@ -52,6 +53,7 @@ More information
 * We are also happy to pick you up at Jemaa el-Fnaa (phone number will be provided once you registered).
 
 Previous retreats:
+* 6th October 3rd - 10th 2018 in Marrakesh
 * 5th March 7th - 18th 2018 in Marrakesh, reports: [linse](https://linse.me/2018/04/20/Visiting-the-camels.html), [peter](https://mirage.io/wiki/arm64)
 * 4th November 29th - December 6th 2017 in Marrakesh, reports: [mirage](https://mirage.io/blog/2017-winter-hackathon-roundup)
 * 3rd March 1st - 8th 2017 in Marrakesh, reports: [mirage](https://mirage.io/blog/2017-march-hackathon-roundup), [reynir](http://reyn.ir/posts/2017-03-20-11-27-Marrakech%202017.html), [olle](http://ollehost.dk/blog/2017/03/17/travel-report-mirageos-hack-retreat-in-marrakesh-2017/)
