cec848af32d460328837883fa0b17c501c8d269f
Hannes Mehnert <hannes@mehnert.org>
2019-03-02 21:37:34 +0100

    redeploy with 3.5.0

--- config.ml
+++ config.ml
@@ -2,12 +2,14 @@ open Mirage
 
 let net = generic_stackv4 default_network
 
-let logger = syslog_udp ~config:(syslog_config ~truncate:1484 "retreat") net
+let logger = syslog_udp ~config:(syslog_config "retreat") net
 
 let packages = [
   package ~sublibs:["lwt"] "logs" ;
   package "omd" ;
   package "tyxml" ;
+  package ~min:"3.7.1" "tcpip" ;
+  package ~min:"0.2.1" "logs-syslog" ;
 ]
 
 let () =
