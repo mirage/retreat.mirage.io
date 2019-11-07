f0c02e5761456a7eeefa79e8fde2b38c2668c223
Hannes Mehnert <hannes@mehnert.org>
2019-11-07 16:23:03 +0100

    monitoring and syslog on a second device, also http access stats

--- config.ml
+++ config.ml
@@ -1,19 +1,37 @@
 open Mirage
 
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "retreat.mirage.io" doc))
+
 let net = generic_stackv4 default_network
+
+let management_stack = generic_stackv4 ~group:"management" (netif ~group:"management" "management")
 
 let packages = [
   package ~sublibs:["lwt"] "logs" ;
   package "omd" ;
   package "tyxml" ;
   package ~min:"3.7.1" "tcpip" ;
+  package "monitoring-experiments" ;
+  package ~sublibs:["mirage"] "logs-syslog" ;
 ]
 
 let () =
   register "retreat" [
     foreign
+      ~deps:[abstract app_info]
+      ~keys:[ Key.abstract name ; Key.abstract syslog ; Key.abstract monitor ]
       ~packages
       "Unikernel.Main"
-      ( stackv4 @-> job )
-    $ net
+    (console @-> time @-> mclock @-> pclock @-> stackv4 @-> stackv4 @-> job)
+    $ default_console $ default_time $ default_monotonic_clock $ default_posix_clock $ net $ management_stack
   ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,6 +1,6 @@
 open Lwt.Infix
 
-module Main (S : Mirage_stack.V4) = struct
+module Main (C : Mirage_console.S) (T : Mirage_time.S) (M : Mirage_clock.MCLOCK) (P : Mirage_clock.PCLOCK) (S : Mirage_stack.V4) (Management : Mirage_stack.V4) = struct
   module TCP = S.TCPV4
 
   let http_header ~status xs =
@@ -14,13 +14,42 @@ module Main (S : Mirage_stack.V4) = struct
         ("Content-length", string_of_int len) ;
         ("Connection", "close") ]
 
+  let incr_access =
+    let s = ref 0 in
+    let open Metrics in
+    let doc = "access statistics" in
+    let data () =
+      Data.v [
+        int "total http responses" !s ;
+      ] in
+    let src = Src.v ~doc ~tags:Tags.[] ~data "http" in
+    (fun () ->
+       s := succ !s;
+       Metrics.add src (fun x -> x) (fun d -> d ()))
+
   let serve data tcp =
-    let ip, port = TCP.dst tcp in
-    Logs_lwt.info (fun m -> m "%s:%d served" (Ipaddr.V4.to_string ip) port) >>= fun () ->
+    incr_access ();
     TCP.writev tcp data >>= fun _ ->
     TCP.close tcp
 
-  let start stack =
+  module Monitoring = Monitoring_experiments.Make(T)(Management)
+  module Syslog = Logs_syslog_mirage.Udp(C)(P)(Management)
+
+  let start c _time _mclock _pclock stack management info =
+    let hostname = Key_gen.name ()
+    and syslog = Key_gen.syslog ()
+    and monitor = Key_gen.monitor ()
+    in
+    if Ipaddr.V4.compare syslog Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
+    else
+      Logs.set_reporter (Syslog.create c management syslog ~hostname ());
+    if Ipaddr.V4.compare monitor Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
+    else
+      Monitoring.create ~hostname monitor management;
+    List.iter (fun (p, v) -> Logs.app (fun m -> m "used package: %s %s" p v))
+      info.Mirage_info.packages;
     let data =
       let content_size = Cstruct.len Page.rendered in
       [ header content_size ; Page.rendered ]
