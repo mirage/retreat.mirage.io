7e246a4733517b3ddd097fbb739d989568120204
Hannes Mehnert <hannes@mehnert.org>
2023-01-30 13:34:57 +0100

    let's encrypt https

--- config.ml
+++ config.ml
@@ -1,5 +1,21 @@
 open Mirage
 
+let dns_key =
+  let doc = Key.Arg.info ~doc:"nsupdate key (name:type:value,...)" ["dns-key"] in
+  Key.(create "dns-key" Arg.(required string doc))
+
+let dns_server =
+  let doc = Key.Arg.info ~doc:"dns server IP" ["dns-server"] in
+  Key.(create "dns-server" Arg.(required ip_address doc))
+
+let dns_port =
+  let doc = Key.Arg.info ~doc:"dns server port" ["dns-port"] in
+  Key.(create "dns-port" Arg.(opt int 53 doc))
+
+let key =
+  let doc = Key.Arg.info ~doc:"certificate key (<type>:seed or b64)" ["key"] in
+  Key.(create "key" Arg.(required string doc))
+
 let monitor =
   let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
   Key.(create "monitor" Arg.(opt (some ip_address) None doc))
@@ -24,14 +40,20 @@ let packages = [
   package ~min:"3.7.1" "tcpip" ;
   package "mirage-monitoring" ;
   package ~sublibs:["mirage"] ~min:"0.3.0" "logs-syslog" ;
+  package ~min:"6.1.1" ~sublibs:["mirage"] "dns-certify";
+  package "tls-mirage";
+  package ~min:"4.3.1" "mirage-runtime";
 ]
 
 let () =
   register "retreat" [
     foreign
-      ~keys:[ Key.v name ; Key.v syslog ; Key.v monitor ]
+      ~keys:[
+        Key.v dns_key ; Key.v dns_server ; Key.v dns_port ; Key.v key ;
+        Key.v name ; Key.v syslog ; Key.v monitor
+      ]
       ~packages
       "Unikernel.Main"
-    (console @-> time @-> pclock @-> stackv4v6 @-> stackv4v6 @-> job)
-    $ default_console $ default_time $ default_posix_clock $ net $ management_stack
+    (console @-> random @-> time @-> pclock @-> stackv4v6 @-> stackv4v6 @-> job)
+    $ default_console $ default_random $ default_time $ default_posix_clock $ net $ management_stack
   ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,6 +1,9 @@
 open Lwt.Infix
 
-module Main (C : Mirage_console.S) (T : Mirage_time.S) (P : Mirage_clock.PCLOCK) (S : Tcpip.Stack.V4V6) (Management : Tcpip.Stack.V4V6) = struct
+module Main (C : Mirage_console.S) (R : Mirage_random.S) (T : Mirage_time.S) (P : Mirage_clock.PCLOCK) (S : Tcpip.Stack.V4V6) (Management : Tcpip.Stack.V4V6) = struct
+  module Dns_certify = Dns_certify_mirage.Make(R)(P)(T)(S)
+  module TLS = Tls_mirage.Make(S.TCP)
+
   let http_header ~status xs =
     let headers = List.map (fun (k, v) -> k ^ ": " ^ v) xs in
     let lines = status :: headers @ [ "\r\n" ] in
@@ -30,10 +33,20 @@ module Main (C : Mirage_console.S) (T : Mirage_time.S) (P : Mirage_clock.PCLOCK)
     S.TCP.writev tcp data >>= fun _ ->
     S.TCP.close tcp
 
+  let serve_tls cfg data tcp_flow =
+    incr_access ();
+    TLS.server_of_flow cfg tcp_flow >>= function
+    | Ok tls_flow ->
+      TLS.writev tls_flow data >>= fun _ ->
+      TLS.close tls_flow
+    | Error e ->
+      Logs.warn (fun m -> m "TLS error %a" TLS.pp_write_error e);
+      S.TCP.close tcp_flow
+
   module Monitoring = Mirage_monitoring.Make(T)(P)(Management)
   module Syslog = Logs_syslog_mirage.Udp(C)(P)(Management)
 
-  let start c _time _pclock stack management =
+  let start c _random _time _pclock stack management =
     let hostname = Key_gen.name () in
     (match Key_gen.syslog () with
      | None -> Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
@@ -41,10 +54,33 @@ module Main (C : Mirage_console.S) (T : Mirage_time.S) (P : Mirage_clock.PCLOCK)
     (match Key_gen.monitor () with
      | None -> Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
      | Some ip -> Monitoring.create ~hostname ip management);
-    let data =
-      let content_size = Cstruct.length Page.rendered in
-      [ header content_size ; Page.rendered ]
+    let hostname = Domain_name.(host_exn (of_string_exn hostname)) in
+    let key_type, key_data, key_seed =
+      match String.split_on_char ':' (Key_gen.key ()) with
+      | [ typ ; data ] ->
+        (match X509.Key_type.of_string typ with
+         | Ok `RSA -> `RSA, None, Some data
+         | Ok x -> x, Some data, None
+         | Error `Msg msg ->
+           Logs.err (fun m -> m "Error decoding key type: %s" msg);
+           exit Mirage_runtime.argument_error)
+      | _ ->
+        Logs.err (fun m -> m "expected for key type:data");
+        exit Mirage_runtime.argument_error
     in
-    S.TCP.listen (S.tcp stack) ~port:80 (serve data) ;
-    S.listen stack
+    Dns_certify.retrieve_certificate
+      stack ~dns_key:(Key_gen.dns_key ())
+      ~hostname ~key_type ?key_data ?key_seed
+      (Key_gen.dns_server ()) (Key_gen.dns_port ()) >>= function
+    | Error (`Msg m) -> Lwt.fail_with m
+    | Ok certificates ->
+      let certificates = `Single certificates in
+      let tls_config = Tls.Config.server ~certificates () in
+      let data =
+        let content_size = Cstruct.length Page.rendered in
+        [ header content_size ; Page.rendered ]
+      in
+      S.TCP.listen (S.tcp stack) ~port:80 (serve data) ;
+      S.TCP.listen (S.tcp stack) ~port:443 (serve_tls tls_config data);
+      S.listen stack
 end
