82a9a156370ed8a8b71861845a0f948a2c63ed49
Hannes Mehnert <hannes@mehnert.org>
2024-02-05 20:05:52 +0100

    fix link (thx @leostera)

--- page.ml
+++ page.ml
@@ -42,7 +42,7 @@ The goal is to sync various MirageOS subprojects, start new ones, and fix bugs.
 
 More information
 * Once you've signed up, you will subscribed to the participants mailing list.
-* You can work on anything but if you need inspiration, browse the [open issues](https://github.com/search?q=is%3Aopen+org%3Amirage).
+* You can work on anything but if you need inspiration, browse the [open issues](https://github.com/search?q=is%3Aopen+org%3Amirage&type=issues).
 * The nearest airport is [Marrakesh Menara Airport (RAK)](https://en.wikipedia.org/wiki/Marrakesh_Menara_Airport).  There is also Marrakesh Railway Station (train service from and to Tangier, reachable from Spain by ferry).
 * From airport or railway station, take a cab to **Jemaa el-Fnaa** (city centre).
 * Follow the [map](https://raw.githubusercontent.com/mirage/marrakech2017/master/data/map.jpg), the address is **27 Derb el Ferrane Azbezt**.
